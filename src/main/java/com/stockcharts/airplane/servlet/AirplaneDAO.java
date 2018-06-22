c8f689fa12cce78aa53dae14abd387154a9a1976
Emily Phelps <phelps.emily@gmail.com>
2018-06-22 16:58:40 -0700

    airplaneDAO and airplaneServlet

--- src/main/java/com/stockcharts/airplane/servlet/AirplaneDAO.java
+++ src/main/java/com/stockcharts/airplane/servlet/AirplaneDAO.java
@@ -1,20 +1,38 @@
 package com.stockcharts.airplane.servlet;
 
+import com.stockcharts.commons.net.*;
+import java.io.IOException;
 import java.util.*;
 import java.util.logging.Logger;
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 public class AirplaneDAO {
     
     private static final Logger logger = Logger.getLogger(AirplaneDAO.class.getName());
     
-    public static List<Airplane> getAirplanesFromFee()
+    public static List<Airplane> getAirplanesFromFee() throws IOException
     {
+        RestResponse response = new RestRequest(AirplaneServlet.AIRPLANE_FEED_URL).doGet();
+        
+        String responseBody = response.getBody();
+        
+        JSONObject jo = new JSONObject(response.getBody());
+        JSONArray features = jo.getJSONArray("features");
+        
         List<Airplane> airplanes = new ArrayList<>();
+        
+        for(int i = 0 ; i < features.length(); i++)
+        {
+            JSONObject o = features.getJSONObject(i);
+            
+            airplanes.add(getAirplaneFromJSONObject(o));
+        }
         
         return airplanes;
     }
     
-    private static Airplane getAirplaneFromJSONObject()
+    private static Airplane getAirplaneFromJSONObject(JSONObject jo) throws IOException
     {
         Airplane airplane = new Airplane();
         
---
+++ src/main/java/com/stockcharts/airplane/servlet/AirplaneServlet.java
@@ -0,0 +1,75 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.stockcharts.airplane.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+import java.util.logging.Logger;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.UnavailableException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import com.google.common.cache.*;
+
+
+public class AirplaneServlet extends HttpServlet {
+
+        private static final Logger logger = Logger.getLogger(AirplaneServlet.class.getName());
+
+    public static final String AIRPLANE_FEED_URL = "https://public-api.adsbexchange.com/VirtualRadar/AircraftList.json?fTypQN=";
+
+    private Cache<String, List<Airplane>> earthquakeCache;
+    
+    @Override
+    public void init(ServletConfig config) throws ServletException
+    {
+        super.init(config);
+        
+        logger.warn("==================================================");
+        logger.warn("           airplane-servlet : init()");
+        logger.warn("==================================================");
+        
+        logger.warn("Setting up Guava Cache...");
+        earthquakeCache = CacheBuilder.newBuilder()
+                .expireAfterAccess(30, TimeUnit.SECONDS)
+                .build();
+        logger.warn("...success");
+        
+        logger.warn("==================================================");
+        logger.warn("           airplane-servlet : init() - COMPLETE");
+        logger.warn("==================================================");
+    }
+    
+    @Override
+    public void destroy()
+    {
+        logger.warn("<<<<<<<<<<<<<<<<<<<< ######## >>>>>>>>>>>>>>>>>>>>");
+        logger.warn("           airplane-servlet : destroy()");
+        logger.warn("<<<<<<<<<<<<<<<<<<<< ######## >>>>>>>>>>>>>>>>>>>>");
+        
+        logger.warn("<<<<<<<<<<<<<<<<<<<< ######## >>>>>>>>>>>>>>>>>>>>");
+        logger.warn("           airplane-servlet : destroy() - COMPLETE");
+        logger.warn("<<<<<<<<<<<<<<<<<<<< ######## >>>>>>>>>>>>>>>>>>>>");
+        
+    }
+    
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+       
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        
+    }
+    
+}
